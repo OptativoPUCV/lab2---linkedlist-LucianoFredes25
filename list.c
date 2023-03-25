30fb715bbae25e33beeec23a51d0efb76828d69d
Replit user <>
2023-03-25 15:15:29 +0000

    new try

--- list.c
+++ list.c
@@ -68,7 +68,7 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, void * data) {
-  Node * Aux = createnode(data);
+  Node * Aux = createNode(data);
 }
 
 void pushBack(List * list, void * data) {
