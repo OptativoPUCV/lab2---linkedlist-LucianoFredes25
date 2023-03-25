a29981eb9bb20aa61e13d3ec4c8665a1b68d88cf
Replit user <>
2023-03-25 15:34:03 +0000

    new try

--- list.c
+++ list.c
@@ -90,7 +90,7 @@ void pushCurrent(List * list, void * data) {
   Node * Aux = createNode(data);
   Node * AuxNext = list->next;
   list->next = Aux;
-  printf("%p", list->next->data);
+  printf("%d", list->next->data);
   Aux->next = AuxNext;
 }
 
